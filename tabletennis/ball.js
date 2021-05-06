e10b2d9ebb6d528e5ec0f3cc8a3499280b310c45
jaredfeuer20 <jmfeuer@emory.edu>
2021-05-06 17:09:28 -0400

    fixed the scoring and whole system

--- tabletennis/ball.js
+++ tabletennis/ball.js
@@ -1,38 +1,51 @@
-class Ball {
-    constructor(radius) {
+class Ball {    
+    constructor(radius, mx, my) {
         this.r =radius;
         this.x = 0;
         this.y = 0;
         this.speed = 10;
+        this.moveX = mx;
+        this.moveY = my;
     }
 
-
+    flipdirection() {
+        this.moveX *= -1;
+    }
 
     setLocation(newX, newY) {
         this.x = newX;
         this.y = newY;
     }
 
-
     display() { // draw the ball and make it bounce off paddles
         stroke(0);
         fill(0, 125, 255);
         ellipse(this.x, this.y, this.r*2);
-        x = x + moveX;
-        y = y + moveY;
-        if ((x > width)) { //|| (x <= 0)) 
-            moveX *= -1;
+        this.x = this.x + this.moveX;
+        this.y = this.y + this.moveY;
+        if ((this.x > width)) { //|| (this.x <= 0)) 
+            this.moveX *= -1;
         }
-        if ((y > height) || (y <= 0)) {
-            moveY *= -1;
+        if ((this.y > height) || (this.y <= 0)) {
+            this.moveY *= -1;
         }
        // collison with left (user) paddle
         if ((this.x < 20) && ((this.y > mouseY && this.y < mouseY + 100)
         )) {
-            moveX *= -1;
-            x = x + Math.abs((this.x - 20)*2);
+            this.moveX *= -1;
+            this.x = this.x + Math.abs((this.x - 20)*2); // to make the ball actually come off paddle
         }
+        
+        //generate points for user or computer
+        if (this.x < 0) {
+            scorecom += 1;
+        }
+        if (this.x - this.r > 800) {
+            scoreuser += 1;
+        }
+        
     }
+    
   
 }
 
--- tabletennis/sketch.js
+++ tabletennis/sketch.js
@@ -3,20 +3,25 @@ var paddles;
 var paddles2;
 var x = 400;
 var y = 400;
-var moveX = 4;
-var moveY = 6;
 var scoreuser = 0;
 var scorecom = 0;
-
-
-
+var resetcounter = 0;
 
 function setup() {
 	createCanvas(800, 800);
-	ball = new Ball(15);
+	ball = new Ball(15, 4, 6);
 	paddles = new Paddles(15);
 	paddles2 = new Paddles2(15);
+	ball.setLocation(x, y); // set ball up in middle
 }
+
+function myreset() {
+	ball.setLocation(x, y); // set ball up in middle
+	this.resetcounter ++;
+	ball.flipdirection();
+}
+
+
 
 function draw() {
 background(0);
@@ -24,31 +29,20 @@ stroke(255);//color of line
 strokeWeight(5); // line thickness
 line(width/2 ,0,width/2,800);
 
-ball.setLocation(x, y); // set ball up in middle
+
 ball.display();
 
 
 paddles.display();
 paddles2.display();
 
-if (this.x + this.r < 0) {
-	ball.reset();
-	scorecom ++;
-}
-if (this.x - this.r > 800) {
-	ball.reset();
-	scoreuser ++;
-}
-this.reset = function() {
-	this.r =radius;
-	this.x = 0;
-	this.y = 0;
-	this.speed = 10;
+if (ball.x < 0) {
+	myreset();
 }
 
-
+print(x," ", y);
 text(24);
 text("User Score:  " + int(scoreuser) + "  Computer Score:  " + int(scorecom), 300, 50); // add score
-
 	
 }
+
